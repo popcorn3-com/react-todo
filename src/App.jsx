bd582051f1759d250088c62dfb4e088d55d2bee6
Hiro <sgi78ks@popcorn3.com>
2022-04-16 09:30:50 +0900

    style

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import "./styles.css";
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input type="text" placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
-        <ul>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul className="to-do-list">
           <li>
             <p>ああああああああ</p>
             <button>完了</button>
@@ -22,9 +22,9 @@ export const App = () => {
           </li>
         </ul>
       </div>
-      <div>
-        <p>完了のTODO</p>
-        <ul>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul className="to-do-list">
           <li>
             <p>うううう</p>
             <button>戻す</button>
